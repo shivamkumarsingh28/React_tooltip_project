44c02186a5288a70b9b224e93b2d3e021af69a26
shivamkumarsingh28 <saeeamsingh@gmail.com>
2023-08-05 10:24:18 +0530

    React toolkit project

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Button extends Component {
+  render() {
+    return (
+        <div>
+            <button className='btn btn-primary'>Hover Over Me !</button>
+        </div>
+    )
+  }
+}
